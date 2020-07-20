596894455f04d66082a0e950faaaa3231e93ec05
AsherLecover <asherlec5@gmail.com>
2020-07-20 20:18:39 +0300

    initail commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'app-sign-in', component:SignInComponent},
+  { path:'',   redirectTo:'app-sign-in', pathMatch: 'full'},
+  { path:'**', redirectTo:'app-sign-in', pathMatch: 'full'},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModuleMatrialModule } from './module-matriel/module-matriel.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ModuleMatrialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
